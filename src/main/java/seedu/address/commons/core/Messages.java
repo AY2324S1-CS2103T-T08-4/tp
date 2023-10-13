985e159eceac13bc3b469841cb32c942b2380f71
billieboy7 <smkbillkirankumar@gmail.com>
2023-10-13 14:17:57 +0800

    Add Appointment Messages

---
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,9 @@
+package seedu.address.commons.core;
+
+public class Messages {
+
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_APPOINTMENT_SLOT = "That time slot is already taken.";
+    public static final String MESSAGE_DUPLICATE_APPOINTMENT = "This appointment already exists in the scheduling" +
+            "database. Please choose another scheduling slot.";
+}
