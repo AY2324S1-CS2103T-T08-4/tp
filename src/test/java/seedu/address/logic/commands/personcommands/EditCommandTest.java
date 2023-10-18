081de909144447c2c1c0003c05faee93c4a44634
billieboy7 <smkbillkirankumar@gmail.com>
2023-10-18 20:00:06 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/personcommands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/personcommands/EditCommandTest.java
@@ -124,7 +124,8 @@ public class EditCommandTest {
     @Test
     public void execute_invalidPersonIndexUnfilteredList_failure() {
         Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPatientList().size() + 1);
-        EditCommand.EditPatientDescriptor descriptor = new EditPatientDescriptorBuilder().withName(VALID_NAME_BOB).build();
+        EditCommand.EditPatientDescriptor descriptor = new EditPatientDescriptorBuilder()
+                .withName(VALID_NAME_BOB).build();
         EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
 
         assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
